34ead357fd48f8bf0810864252efe6662def6a4b
RayFaybian <fabian.suppan@hotmail.com>
2020-02-21 21:37:14 +0100

    added encryption

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Goodbye World!");
+
+        // Verschlüssle diese Nachricht: "DASISTEINEGEHEIMENACHRICHT"
+        // Nutze die Cäsar Verschlüsselung zb. A -> B/ X -> Y
+
+
+        String message = "DASISTEINEGEHEIMENACHRICHT";
+
+        char[] messageEncryption = message.toCharArray();
+        int length = message.length();
+        int counter = 0;
+
+        while (counter < length){
+
+        System.out.print(messageEncryption[counter] += 1);
+        counter++;}
+
     }
 }
