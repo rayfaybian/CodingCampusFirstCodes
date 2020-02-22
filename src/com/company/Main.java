ef6b0f0499c496794e9e9b872cf86533e73431b4
RayFaybian <fabian.suppan@hotmail.com>
2020-02-22 11:18:25 +0100

    - added decryption  - changed while loop to for loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,16 +9,35 @@ public class Main {
 
 
         String message = "DASISTEINEGEHEIMENACHRICHT";
-
         char[] messageEncryption = message.toCharArray();
-        int length = message.length();
-        int counter = 0;
+        int lengthMessage = message.length();
+        String encryptedMessage = "";
+        String decryptedMessage = "";
 
-        while (counter < length){
 
-        System.out.print(messageEncryption[counter] += 1);
-        counter++;}
+        System.out.println("\n");
 
+        System.out.print("Verschlüsselt: ");
+
+        for (int i = 0; i < lengthMessage; i++) {
+            char encryptedChar = messageEncryption[i] += 1;
+            encryptedMessage = encryptedMessage + Character.toString(encryptedChar);
+
+        }
+        System.out.println(encryptedMessage + "\n");
+
+        char[] messageDecryption = encryptedMessage.toCharArray();
+
+        System.out.print("Entschlüsselt: ");
+
+        int lengthEncryptedMessage = encryptedMessage.length();
+
+        for (int i = 0; i < lengthEncryptedMessage; i++) {
+            char decryptedChar = messageDecryption[i] -= 1;
+            decryptedMessage = decryptedMessage + Character.toString(decryptedChar);
+        }
+
+        System.out.println(decryptedMessage);
 
     }
 }
