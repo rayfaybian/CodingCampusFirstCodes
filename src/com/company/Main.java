f11210a99063774354317c3086d9e8f830de42e4
RayFaybian <fabian.suppan@hotmail.com>
2020-02-22 13:00:09 +0100

    Version 1.1.6
    new features: more ugly code!
                  costum sentences. type in your own sentence to encrypt.
    
                              ---added scanner for userinput.---
    
    symbol detection: symbols (!,?; etc.) will not be encrypted*
    
                        ---added an if statement for symbol detection---
    
    * only in PRO Version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,9 +9,12 @@ public class Main {
         // Verschlüssle diese Nachricht: "DASISTEINEGEHEIMENACHRICHT"
         // Nutze die Cäsar Verschlüsselung zb. A -> B/ X -> Y
 
+        Scanner userInput = new Scanner(System.in);
 
-        String message = "DASISTEINEGEHEIMENACHRICHT";
-        char[] messageEncryption = message.toCharArray();
+        System.out.println("Geben Sie Ihre zu verschlüsselnde Nachricht ein.");
+        String message = userInput.nextLine();
+
+        char[] messageEncryption = message.toCharArray();//Der String message wird in ein Character Array gespeichert
         int lengthMessage = message.length();
         String encryptedMessage = "";
         String decryptedMessage = "";
@@ -20,24 +25,42 @@ public class Main {
         System.out.print("Verschlüsselt: ");
 
         for (int i = 0; i < lengthMessage; i++) {
-            char encryptedChar = messageEncryption[i] += 1;
-            encryptedMessage = encryptedMessage + Character.toString(encryptedChar);
-
+            char encryptedChar;
+            if (((messageEncryption[i] == ('!')) || (messageEncryption[i] == ('?')) ||
+                    (messageEncryption[i] == ('.')) || (messageEncryption[i] == (',')) ||
+                    (messageEncryption[i] == (';')) || (messageEncryption[i] == (':')) || (messageEncryption[i] == (' ')))) {
+                encryptedChar = messageEncryption[i];
+                encryptedMessage = encryptedMessage + Character.toString(encryptedChar);
+            } else {
+                encryptedChar = messageEncryption[i] += 7;
+                encryptedMessage = encryptedMessage + Character.toString(encryptedChar);
+            }
         }
+
+
+        char[] messageDecryption = encryptedMessage.toCharArray(); //Die verschlüsselte Nachricht wird in ein neues Array gespeichert
         System.out.println(encryptedMessage + "\n");
 
-        char[] messageDecryption = encryptedMessage.toCharArray();
 
         System.out.print("Entschlüsselt: ");
 
         int lengthEncryptedMessage = encryptedMessage.length();
 
-        for (int i = 0; i < lengthEncryptedMessage; i++) {
-            char decryptedChar = messageDecryption[i] -= 1;
-            decryptedMessage = decryptedMessage + Character.toString(decryptedChar);
+        for (int j = 0; j < lengthEncryptedMessage; j++) {
+            char decryptedChar;
+            if (((messageDecryption[j] == ('!')) || (messageDecryption[j] == ('?')) ||
+                    (messageDecryption[j] == ('.')) || (messageDecryption[j] == (',')) ||
+                    (messageDecryption[j] == (';')) || (messageDecryption[j] == (':')) || (messageDecryption[j] == (' ')))) {
+                decryptedChar = messageDecryption[j];
+                decryptedMessage = decryptedMessage + Character.toString(decryptedChar);
+            } else {
+                decryptedChar = messageDecryption[j] -= 7;
+                decryptedMessage = decryptedMessage + Character.toString(decryptedChar);
+            }
         }
-
         System.out.println(decryptedMessage);
-
     }
 }
+
+
+
